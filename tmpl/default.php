7934d15b8237f3d83372079b35b1d880dc3e97dd
Gyurltrini <kathryn.ma@gmail.com>
2021-01-28 09:51:01 -0400

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -3,6 +3,7 @@
 defined('_JEXEC') or die; ?>
 <?php 
 $currentDateTime = date('Y-m-d H:i:s');
+echo 'The maximum number of entries to list are: '.$params->get('maxentries','1');
 foreach($phocadownload as $download){
 	//echo ' Title: '.$download['0'].' CatID: '.$download['1'].' PDF: '.$download['2'].' Pup: '.$download['3'].' PDown:'.$download['4'].'<br/>';
 	if($currentDateTime>=$download['3']){//since I could not do this in the sql statement I'm doing this here ... if current date is later than or equal to the publish_up date
