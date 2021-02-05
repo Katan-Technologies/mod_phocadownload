4a987d8acac5fe1b9611102c1c3ea33e5979868e
Gyurltrini <kathryn.ma@gmail.com>
2021-02-05 13:45:12 -0400

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -8,7 +8,7 @@ $currentDateTime = date('Y-m-d H:i:s');
 $count = 1;
 //echo count($phocadownload).'<br/>';
 foreach($phocadownload as $download){
-//	echo ' Title: '.$download['0'].' Alias: '.$download['1'].' ID: '.$download['2'].' CatID: '.$download['3'].' PDF: '.$download['4'].' Pup: '.$download['5'].' PDown:'.$download['6'].' Access level: '.$download['7'].'<br/>';
+	echo ' Title: '.$download['0'].' Job Alias: '.$download['1'].' ID: '.$download['2'].' CatID: '.$download['3'].' PDF: '.$download['4'].' Pup: '.$download['5'].' PDown:'.$download['6'].' Access level: '.$download['7'].' Category Alias: '.$download['8'].'<br/>';
 //	echo 'current date / time is less than or equal to the publish down time: '.$currentDateTime<=$download['6'].'<br/>';
 //	echo $download['6'];//==new DateTime('0000-00-00 00:00:00');
 /*	if (strtotime($currentDateTime)<=strtotime($download['6'])){
@@ -18,7 +18,7 @@ foreach($phocadownload as $download){
 		echo 'publish down time is not set<br/>';
 	}*/
 //	if (strtotime($currentDateTime)>=strtotime($download['5'])){echo 'it is greater!<br/>';}
-	echo 'Category is '.$cat.'</br>';
+//	echo 'Category is '.$cat.'</br>';
 	if(strtotime($currentDateTime)>=strtotime($download['5'])&&((strtotime($currentDateTime)<=strtotime($download['6']))||(strtotime($download['6'])==strtotime('0000-00-00 00:00:00')))){//since I could not do this in the sql statement I'm doing this here ... if current date is later than or equal to the publish_up date
 		echo 'title: '.$download['0'].'<br/>';
 //			echo 'count: '.$count.'<br/>';
