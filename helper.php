55b07fb1cb173bbfa7246c2cc234eb0b9021a004
Gyurltrini <kathryn.ma@gmail.com>
2021-02-09 13:46:50 -0400

    Update helper.php

--- helper.php
+++ helper.php
@@ -24,25 +24,51 @@ class ModPhocaDownloadHelper
     public static function getDownloads($params)
     {
 		// Obtain a database connection
-		$db = JFactory::getDbo();
+//		$db = JFactory::getDbo();
 		// Retrieve the shout. Note that we are now retrieving the id not the lang field.
 /*		$query = $db->getQuery(true)
 					->select($db->quoteName(array('title','alias','id','catid','filename','publish_up','publish_down','access')))//should also get the category id and what ever fields are needed to associate with the pdf file (catid, filename,filename_preview)
 					->from($db->quoteName('#__phocadownload'))
 					->where ('catid = '.$db->Quote($params).' AND published=1')//were curdate() gets the current date ... could not get comparison between publish_up and publish_down to work in the same clause
 					->order ('publish_up DESC');*/
-	    	$query
- 		->select($db->quoteName(array('a.title', 'a.alias', 'a.id', 'a.catid', 'a.filename', 'a.publish_up', 'a.publish_down', 'a.access', 'b.id', 'b.alias')))
- 		->from($db->quoteName('#__phocadownload', 'a'))
- 		->join('INNER', $db->quoteName('#__phocadow3nload_categories', 'b') . ' ON (' . $db->quoteName('a.catid') . ' = ' . $db->quoteName('b.id') . ')')
- 		->where($db->quoteName('b.id') . ' LIKE \'z%\'')
- 		->order($db->quoteName('a.publish_up') . ' DESC');
+//	    	$query
+// 		->select($db->quoteName(array('a.title', 'a.alias', 'a.id', 'a.catid', 'a.filename', 'a.publish_up', 'a.publish_down', 'a.access', 'b.id', 'b.alias')))
+// 		->from($db->quoteName('#__phocadownload', 'a'))
+// 		->join('INNER', $db->quoteName('#__phocadow3nload_categories', 'b') . ' ON (' . $db->quoteName('a.catid') . ' = ' . $db->quoteName('b.id') . ')')
+// 		->where($db->quoteName('b.id') . ' LIKE \'z%\'')
+// 		->order($db->quoteName('a.publish_up') . ' DESC');
  
 		// Prepare the query
-		$db->setQuery($query);
+//		$db->setQuery($query);
 		// Load the row.
 		
-		$results = $db->loadRowList();
+//		$results = $db->loadRowList();
+	    // Get a db connection.
+$db = JFactory::getDbo();
+
+// Create a new query object.
+$query = $db->getQuery(true);
+
+// Select all articles for users who have a username which starts with 'a'.
+// Order it by the created date.
+// Note by putting 'a' as a second parameter will generate `#__content` AS `a`
+$query
+    //->select(array('a.*', 'b.title', 'b.alias')/*,array('title', 'cattitle', 'catalias')*/)
+	->select(array('a.title', 'a.alias','a.id', 'a.catid', 'a.filename', 'a.publish_up', 'a.publish_down', 'a.access', ))
+	->select($db->quoteName(array('b.title','b.alias'), array('catTitle','catAlias')))
+    ->from($db->quoteName('#__phocadownload', 'a'))
+    ->join('INNER', $db->quoteName('#__phocadownload_categories', 'b') . ' ON ' . $db->quoteName('a.catid') . ' = ' . $db->quoteName('b.id'))
+    ->where($db->quoteName('b.id') . ' LIKE ' . $db->quoteName('a.catid'))
+    ->order($db->quoteName('a.publish_up') . ' DESC');
+
+// Reset the query using our newly populated query object.
+$db->setQuery($query);
+
+// Load the results as a list of stdClass objects (see later for more options on retrieving data).
+$results = $db->loadObjectList();
+	    return $results;
+   return $results;
+    }
 		
 		// Return the Hello
 		//print_r($result);
