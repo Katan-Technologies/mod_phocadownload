31c826b41b82e3579c2fe2bc417f152d46607e07
Gyurltrini <kathryn.ma@gmail.com>
2023-02-05 12:31:59 -0400

    Update default.php

--- tmpl/default.php
+++ tmpl/default.php
@@ -4,6 +4,11 @@ defined('_JEXEC') or die; ?>
 <?php 
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Uri\Uri;
+if (!class_exists('PhocaDownloadFile')) {
+	include("administrator/components/com_phocadownload/libraries/phocadownload/file/file.php");
+	include("administrator/components/com_phocadownload/libraries/phocadownload/path/path.php");
+	include("administrator/components/com_phocadownload/libraries/phocadownload/utils/settings.php");
+}
 $uri = Uri::getInstance();
 $url = $uri->toString();
 $currentDateTime = date('Y-m-d H:i:s');
@@ -24,18 +29,19 @@ foreach($phocadownload as $download){
 		$compURL = JURI::root().'index.php?option=com_phocadownload&view=category&id='.$download['3'].'&Itemid=367&lang=en';
 	}
 	elseif(gettype($download)=='object'){
-    		$phocadownldfl = new PhocaDownloadFile;
+		$phocadownldfl = new PhocaDownloadFile;
 		$pdurl = Uri::root().'index.php/downloads?download='.$download->id.':'.$download->alias;
-    		if(strtotime($currentDateTime)>=strtotime($download->publish_up)&&((strtotime($currentDateTime)<=strtotime($download->publish_down))||(strtotime($download->publish_down)==strtotime('0000-00-00 00:00:00')))){//since I could not do this in the sql statement I'm doing this here ... if current date is later than or equal to the publish_up date
+		if(strtotime($currentDateTime)>=strtotime($download->publish_up)&&((strtotime($currentDateTime)<=strtotime($download->publish_down))||(strtotime($download->publish_down)==strtotime('0000-00-00 00:00:00')))){//since I could not do this in the sql statement I'm doing this here ... if current date is later than or equal to the publish_up date
 			$filesize = $phocadownldfl->getFileSize($download->filename);
-        		echo '<li><a href="'.$pdurl.'"><i class="fa-solid fa-file-pdf"></i>  '.$download->title.' ['.$filesize.' ] <i class="fa-solid fa-download"></i><a/></li>';
+			echo '<li><a href="'.$pdurl.'"><div class="pdm-title"><i class="fa-solid fa-file-pdf"></i>  '.$download->title.' </div><div class="pdm-dwn"><span>['.$filesize.' ]</span> <i class="fa-solid fa-download"></i></div><a/></li>';
 			if($maxentries!=0 && $maxentries==$count){
 				break;
 			}
 			$count++;
 		}
 		$compURL = JURI::root().'index.php?option=com_phocadownload&view=category&id='.$download->catid.'&Itemid=367&lang=en';
-    	}
+    		
+    }
 }
 echo '</ul>';
 echo '<p style="background-color: orange; border-radius: 10px; padding: 5px 10px 5px 10px; margin-top: 10px; display: inline-flex"><a style="color: white; font-weight: bold; font-size: smaller" href="'.$compURL.'">View & Download All</a></p>';
